d439d95e783a2d3305c5b40a9b404a6e53081a37
Yudai Odagiri <ixixizko@gmail.com>
2011-10-18 00:41:15 +0900

    multi region

--- lib/fluent/plugin/out_sqs.rb
+++ lib/fluent/plugin/out_sqs.rb
@@ -15,6 +15,7 @@ module Fluent
         config_param :aws_key_id, :string
         config_param :aws_sec_key, :string
         config_param :queue_name, :string
+        config_param :sqs_endpoint, :string, :default => 'sqs.ap-northeast-1.amazonaws.com'
 
 
         def configure(conf)
@@ -23,10 +24,15 @@ module Fluent
 
         def start
             super
-            @sqs = AWS::SQS.new(
+            p @sqs_endpoint
+            AWS.config(
                 :access_key_id => @aws_key_id,
-                :secret_access_key => @aws_sec_key )
-                @queue = @sqs.queues.create(@queue_name)
+                :secret_access_key => @aws_sec_key,
+                :sqs_endpoint => @sqs_endpoint )
+
+            @sqs = AWS::SQS.new
+            @queue = @sqs.queues.create(@queue_name)
+
         end
 
         def shutdown
