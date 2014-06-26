705a1c5faecc857507920994330e9b032fb65f93
y13i <email@y13i.com>
2014-06-26 11:12:38 +0900

    set nil to aws_key_id and aws_sec_key by default.

--- lib/fluent/plugin/out_sqs.rb
+++ lib/fluent/plugin/out_sqs.rb
@@ -12,22 +12,22 @@ module Fluent
         include SetTimeKeyMixin
         config_set_default :include_time_key, true
 
-        config_param :aws_key_id, :string
-        config_param :aws_sec_key, :string
+        config_param :aws_key_id, :string, :default => nil
+        config_param :aws_sec_key, :string, :default => nil
         config_param :queue_name, :string
         config_param :sqs_endpoint, :string, :default => 'sqs.ap-northeast-1.amazonaws.com'
         config_param :delay_seconds, :integer, :default => 0
         config_param :include_tag, :bool, :default => true
         config_param :tag_property_name, :string, :default => '__tag'
         #config_param :buffer_queue_limit, :integer, :default => 10
-        
+
         def configure(conf)
             super
         end
 
         def start
             super
-            
+
             AWS.config(
                 :access_key_id => @aws_key_id,
                 :secret_access_key => @aws_sec_key)
@@ -35,13 +35,13 @@ module Fluent
             @sqs = AWS::SQS.new(
                 :sqs_endpoint => @sqs_endpoint)
             @queue = @sqs.queues.create(@queue_name)
-            
+
         end
 
         def shutdown
             super
         end
-        
+
         def format(tag, time, record)
             if @include_tag then
                 record[@tag_property_name] = tag
@@ -49,7 +49,7 @@ module Fluent
 
             record.to_msgpack
         end
-        
+
         def write(chunk)
             records = []
             chunk.msgpack_each {|record| records << { :message_body => record.to_json, :delay_seconds => @delay_seconds } }
